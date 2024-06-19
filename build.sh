ecd76aca81165ab58a2e88c432cf739337a30020
Iván Monges <64696426+AMongesB@users.noreply.github.com>
2024-06-19 08:50:11 -0300

    Cambiado fastlane

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ script_path=$(cd $(dirname ${0}); pwd)
 cp -r ${script_path}/fastlane ./
 cp ${script_path}/Gemfile ./
 
-bundle add fastlane --version ${FASTLANE_VERSION}
-
 if [[ $BROWSERSTACK_UPLOAD = true || $BUILD_PODS = true ]]; then
   bundle add cocoapods
 fi
@@ -15,8 +13,8 @@ bundle install
 # If the variable FASTLANE_ENV is set then run fastlane with the --env equal to the variable.
 if [ -n "${FASTLANE_ENV}" ]; then
     echo "Running fastlane with environment: ${FASTLANE_ENV}"
-    fastlane --env ${FASTLANE_ENV} build
+    bundle exec fastlane --env ${FASTLANE_ENV} build
 else
     echo "Running fastlane"
-    fastlane build
+    bundle exec fastlane build
 fi
